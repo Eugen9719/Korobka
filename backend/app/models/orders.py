efa543fc622e35ea6ee9845b9a2548f67ac4726a
eugen <slavsep97@gmail.com>
2024-12-16 16:44:06 +0200

    add schema order

--- backend/app/models/orders.py
+++ backend/app/models/orders.py
@@ -13,7 +13,7 @@ class OrderBase(SQLModel):
     # Заказ будет содержать информацию о статусе, общей цене и времени оформления
     status: str = Field(default=StatusEnum.PENDING, max_length=50)  # Статус заказа: pending, completed, canceled
 
-    created_at: datetime = Field(default_factory=datetime.utcnow)  # Время создания заказа
+
 
     @property
     def formatted_created_at(self):
@@ -22,6 +22,7 @@ class OrderBase(SQLModel):
 
 class Order(OrderBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: datetime = Field(default_factory=datetime.utcnow)  # Время создания заказа
     total_price: int  # Общая стоимость заказа
     user_id: int = Field(foreign_key="user.id")
     booking_id: int = Field(foreign_key="booking.id")
@@ -34,16 +35,34 @@ class Order(OrderBase, table=True):
         return f"Заказ №{self.id} для пользователя {self.user_id}, статус: {self.status}"
 
 
-class OrderCreate(SQLModel):
+class OrderCreate(OrderBase):
     booking_id: int  # Идентификатор бронирования
-    status: str = Field(default="pending")  # Статус заказа (по умолчанию "pending")
 
-    # def __init__(self, booking_id: int, status: str = "pending"):
-    #     self.booking_id = booking_id
-    #     self.status = status
-    #     # Вы можете рассчитать total_price, основываясь на информации из бронирования
-    #     booking = booking_repo.get_or_404(id=booking_id)  # Получаем бронирование
-    #     self.total_price = booking.price  # Пример, здесь цена будет из бронирования
+
+class OrderUpdate(OrderBase):
+    pass
+
+
+class UserRead(SQLModel):
+    id: int
+    email:str
+    first_name: str
+    last_name: str
+
+class BookingRead(SQLModel):
+    id: int
+    user: Optional[int]
+    stadium_id: int
+    price: int
+
+class OrderRead(OrderBase):
+    id: Optional[int]
+    created_at: datetime
+    total_price: int
+    user: Optional[UserRead]
+    user_id: int = Field(foreign_key="user.id")
+    booking:Optional[BookingRead]
+
 
 # class OrderAdditionalService(SQLModel, table=True):
 #     id: Optional[int] = Field(default=None, primary_key=True)
